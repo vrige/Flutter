9ba167431fe36a601e87aa70330e011030fab156
vrige <46068335+vrige@users.noreply.github.com>
2023-04-27 09:50:49 +0200

    Create main.dart

---
+++ login_registration/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'welcome_to_the_app_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
